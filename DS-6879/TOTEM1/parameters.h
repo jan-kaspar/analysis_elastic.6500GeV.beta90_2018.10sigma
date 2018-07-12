368ff74958ab7bf9a5a4c0d62abf8718ff3ad21b
Jan Kašpar <jan.kaspar@cern.ch>
2018-07-12 17:38:02 +0200

    Independent parameters for ZeroBias and TOTEM1.

--- DS-6879/TOTEM1/parameters.h
+++ DS-6879/TOTEM1/parameters.h
@@ -1,1 +1,37 @@
-#include "../parameters.h"
+#include "parameters_global.h"
+
+void Init_base()
+{
+	// load global settings
+	Init_global();
+
+	ApplyTOTEM1Settings();
+
+	// list of (sub-)directories with distilled ntuples
+	distilledNtuples.push_back(".");
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45b_56t()
+{
+	Init_global_45b_56t();
+
+	// analysis settings
+	anal.cut1_a = 1.; anal.cut1_c = +3.4E-6; anal.cut1_si = 10E-6;
+	anal.cut2_a = 1.; anal.cut2_c = +0.9E-6; anal.cut2_si = 2.5E-6;
+
+	anal.cut7_a = -1250.; anal.cut7_c = -0.033; anal.cut7_si = 0.01;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45t_56b()
+{
+	Init_global_45t_56b();
+
+	anal.cut1_a = 1.; anal.cut1_c = -8.2E-6; anal.cut1_si = 10E-6;
+	anal.cut2_a = 1.; anal.cut2_c = +1.3E-6; anal.cut2_si = 2.5E-6;
+
+	anal.cut7_a = -1250.; anal.cut7_c = -0.004; anal.cut7_si = 0.01;
+}
--- DS-6885/TOTEM1/parameters.h
+++ DS-6885/TOTEM1/parameters.h
@@ -1,1 +1,37 @@
-#include "../parameters.h"
+#include "parameters_global.h"
+
+void Init_base()
+{
+	// load global settings
+	Init_global();
+
+	ApplyTOTEM1Settings();
+
+	// list of (sub-)directories with distilled ntuples
+	distilledNtuples.push_back(".");
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45b_56t()
+{
+	Init_global_45b_56t();
+
+	// analysis settings
+	anal.cut1_a = 1.; anal.cut1_c = +5.4E-6; anal.cut1_si = 10E-6;
+	anal.cut2_a = 1.; anal.cut2_c = +0.1E-6; anal.cut2_si = 2.5E-6;
+
+	anal.cut7_a = -1250.; anal.cut7_c = -0.048; anal.cut7_si = 0.01;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void Init_45t_56b()
+{
+	Init_global_45t_56b();
+
+	anal.cut1_a = 1.; anal.cut1_c = -5.6E-6; anal.cut1_si = 10E-6;
+	anal.cut2_a = 1.; anal.cut2_c = +0.4E-6; anal.cut2_si = 2.5E-6;
+
+	anal.cut7_a = -1250.; anal.cut7_c = -0.019; anal.cut7_si = 0.01;
+}
